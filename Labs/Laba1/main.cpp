2d382d32b69967da3c1cc7202a1b1e7919670845
VitoScaleto <pushckaryovvitalya404@hmail.com>
2024-09-26 21:07:55 +0300

    add method insert to back, seacrh by value, print list

--- Labs/Laba1/main.cpp
+++ Labs/Laba1/main.cpp
@@ -13,18 +13,29 @@ struct Node
 
 template <typename T>
 
-class LinkedList
+struct LinkedList
 {
 private:
+
     Node<T>* head{ nullptr };
+    int quantity = 0;
+    int index = 0;
 
 public:
-
+    int GetQuantity()
+    {
+        return quantity;
+    }
+    int GetIndex()
+    {
+        return index;
+    }
     void InsertToBack(T item)
     {
         if (head == nullptr)
         {
             head = new Node<T>(item);
+            quantity++;
         }
         else
         {
@@ -32,18 +43,62 @@ public:
             while (current ->next != nullptr)// бегаем по списку пока не наткнемся на конец
             {
                 current = current->next;
+
             }
             current->next = new Node<T>(item);
+            quantity++;
         }
     }
+    void InsertToStart(T item)
+    {
+        if (head == nullptr)
+        {
+            head = new Node<T>(item);
+            quantity++;
+        }
+        else
+        {
+            Node<T>* newNode = new Node<T>(item); 
+            newNode->next = head; 
+            head = newNode; 
+            quantity++;
+        }
 
+    }
+    void SearchByValue(T item)
+    {
+        Node<T>* current = head;
+        while (current != nullptr) 
+        {
+            if (current->value == item) 
+            {
+                cout << "Found value: " << current->value << endl;;
+                return; 
+            }
+            current = current->next; 
+            index++;
+        }
+        throw runtime_error("Error: Item not found"); 
+    }
+
+    void PrintList()
+    {
+        Node<T>* current = head;
+        while (current != nullptr)
+        {
+            cout << current->value << " ";
+            current = current->next;
+            
+        }
+
+    }
 
 };
 
 
 int main()
 {
-   
-  
+    LinkedList<int> intList;
+    
     return 0;
 }
