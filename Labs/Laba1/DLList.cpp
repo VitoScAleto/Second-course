22d7b66a2096a28ed823cfca1585fa87ac7588ca
VitoScaleto <pushckaryovvitalya404@hmail.com>
2024-09-29 18:40:41 +0300

    add push_front and push_back methods

--- Labs/Laba1/DLList.cpp
+++ Labs/Laba1/DLList.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 
 using namespace std;
@@ -9,8 +8,8 @@ struct Node
 {
     T value;
     Node* next;
-    Node* prev;
-    Node(T val) : value(val), next(nullptr), prev(nullptr) {}; // Конструктор для инициализации значения
+    Node* prevToNode;
+    Node(T val) : value(val), next(nullptr), prevToNode(nullptr) {}; // Конструктор для инициализации значения
 };
 
 template <typename T>
@@ -20,17 +19,95 @@ struct DLinkedList
 private:
 
     Node<T>* head{ nullptr };
-    Node<T>* tail{nullptr};
+    Node<T>* tail{ nullptr };
+
     int sizeList = 0;
     int index = 0;
 
 public:
-   
+    void getSize()
+    {
+        cout << sizeList << endl;
+    }
+    void push_back(T item)
+    {
+        Node<T>* currentPtr = new Node<T>(item);
+        if (head == nullptr)
+        {
+            head = currentPtr;
+            sizeList++;
+        }
+        else if (tail == nullptr)
+        {
+            tail = currentPtr;
+            head->next = tail;
+            tail->prevToNode = head;
+            sizeList++;
+        }
+        else
+        {
+            Node<T>* prevPtr = tail;
+            tail = currentPtr;
+            prevPtr->next = tail;
+            tail->prevToNode = prevPtr;
+            sizeList++;
+        }
+    }  
+    
+    void push_front(T item)
+    {
+        Node<T>* currentPtr = new Node<T>(item);
+
+        if (tail == nullptr)
+        {
+            tail = currentPtr;
+            sizeList++;
+        }
+        else if (head == nullptr)
+        {
+            head = currentPtr;
+            head->next = tail;
+            tail->prevToNode = head;
+            sizeList++;
+        }
+        else
+        {
+            Node<T>* prevPtr = head;
+            head = currentPtr;
+            prevPtr->prevToNode = head;
+            head->next = prevPtr;
+            sizeList++;
+        }
+    }
+
+
+    void printListFront()
+    {
+        Node<T>* currentPtr = head;
+        while (currentPtr != nullptr)
+        {
+            cout << currentPtr->value <<" ";
+            currentPtr = currentPtr->next;
+        }
+        cout << endl;
+    }
+    void printListBack()
+    {
+        Node<T>* currentPtr = tail;
+        while (currentPtr != nullptr)
+        {
+            cout << currentPtr->value << " ";
+            currentPtr = currentPtr->prevToNode;
+        }
+        cout << endl;
+    }
 };
+
 
 
 int main()
 {
+    DLinkedList<int> DList;
   
    
     return 0;
