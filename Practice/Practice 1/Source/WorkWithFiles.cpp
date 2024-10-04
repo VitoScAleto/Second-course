2f3daf7cc572817a535144b68beba43bacd4550e
VitoScAleto <pushckaryovvitalya404@gmail.com>
2024-10-04 23:37:16 +0700

    Added fucntion for work with CSV file

--- Practice/Practice 1/Source/WorkWithFiles.cpp
+++ Practice/Practice 1/Source/WorkWithFiles.cpp
@@ -1,5 +1,6 @@
 #include "../Headers/WorkWithFiles.h"
 
+<<<<<<< HEAD
 string ReturnNameObjectFromStructure(int indexObj, json& j)
 {
     string nameObj;
@@ -31,16 +32,26 @@ void WorkWithCSVFile(fs::path pathCSV, json& j)
 
 
 }
+=======
+>>>>>>> 392672e (Added WorkWithFiles.h. Added functionality for working with files)
 
 
 
 void ReadingConfigurationJSON()
 {
+<<<<<<< HEAD
     string pathFileSchemaJson = "/home/pushk/VSCODE/Second-course/Practice/Practice 1/JSON/schema.json";
 
     json j;
 
     ifstream file(pathFileSchemaJson);
+=======
+    string filePath = "/home/pushk/VSCODE/Second-course/Practice/Practice 1/JSON/schema.json";
+
+    json j;
+
+    ifstream file(filePath);
+>>>>>>> 392672e (Added WorkWithFiles.h. Added functionality for working with files)
     if (file.is_open()) 
     {
         // Парсим содержимое файла
@@ -51,15 +62,24 @@ void ReadingConfigurationJSON()
     } 
     else 
     {
+<<<<<<< HEAD
         cerr << "Не удалось открыть файл: " << pathFileSchemaJson << endl;
         return; // Возвращаем код ошибки
     }
     
+=======
+        cerr << "Не удалось открыть файл: " << filePath << endl;
+        return; // Возвращаем код ошибки
+    }
+
+
+>>>>>>> 392672e (Added WorkWithFiles.h. Added functionality for working with files)
 
     string jsonField = j["name"];
 
     fs::path mainDir = jsonField;
 
+<<<<<<< HEAD
     fs::path table1 = mainDir/ReturnNameObjectFromStructure(0, j);
 
     fs::path table2 = mainDir/ReturnNameObjectFromStructure(1, j);
@@ -98,6 +118,38 @@ void ReadingConfigurationJSON()
     catch (const fs::filesystem_error& e) 
     {
         cerr << "Ошибка: " << e.what() << '\n';
+=======
+    jsonField = j["structure"];
+
+    fs::path table1 = mainDir/"jsonField";
+
+    jsonField = j["structure"]["Таблица2"];
+
+    fs::path table2 = mainDir/"jsonField";
+
+    try {
+        // Создание главной директории
+        if (fs::create_directory(mainDir)) {
+            std::cout << "Директория '" << mainDir << "' успешно создана.\n";
+        } else {
+            std::cout << "Директория '" << mainDir << "' уже существует.\n";
+        }
+
+        // Создание поддиректорий
+        if (fs::create_directory(table1)) {
+            std::cout << "Директория '" << table1 << "' успешно создана.\n";
+        } else {
+            std::cout << "Директория '" << table1 << "' уже существует.\n";
+        }
+
+        if (fs::create_directory(table2)) {
+            std::cout << "Директория '" << table2 << "' успешно создана.\n";
+        } else {
+            std::cout << "Директория '" << table2 << "' уже существует.\n";
+        }
+    } catch (const fs::filesystem_error& e) {
+        std::cerr << "Ошибка: " << e.what() << '\n';
+>>>>>>> 392672e (Added WorkWithFiles.h. Added functionality for working with files)
     }
 }
 
